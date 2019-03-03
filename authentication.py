5916f0cb4da83428fda9c9e4f974c1818650d797
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2019-03-03 19:53:56 +0530

    add client credential flow authentication

---
+++ authentication.py
@@ -0,0 +1,12 @@
+import requests, json
+clientId = 'dd976765d83e46bab09dc98ef16d80eb'
+clientSecret = '39ce6898fde54c91987e998cf81b9512'
+
+def getAccessToken():
+    headers = {'Authorization': 'Basic ZGQ5NzY3NjVkODNlNDZiYWIwOWRjOThlZjE2ZDgwZWI6MzljZTY4OThmZGU1NGM5MTk4N2U5OThjZjgxYjk1MTI='}
+    data = {'grant_type': 'client_credentials'}
+    response = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=data)
+    if response.status_code == 200:
+        return json.loads(response.text)
+
+print (getAccessToken())
