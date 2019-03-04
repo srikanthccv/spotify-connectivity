95764880d9a0176ce0c2bf1337eff232fea45820
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2019-03-04 11:21:23 +0530

    change graphGenerator to iterate till end of list

--- graphGenerator.py
+++ graphGenerator.py
@@ -3,26 +3,33 @@ import requests, json, auth
 def go():
     access_token = auth.getAccessToken()
     print (access_token)
+    if access_token is None:
+        raise ValueError('Not a valid access token')
     headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json', 
         'Authorization': 'Bearer ' + access_token
     }
-    offset, limit = 0, 50
+    offset, limit, canProceed = 0, 50, True
     data = {
         'include_groups': 'single',
         'market': 'IN',
         'offset': offset,
         'limit': limit
     }
-    resp = requests.get('https://api.spotify.com/v1/artists/1PDFenKwhb7oNx0bxCyQF2/albums', headers=headers, params=data)
-    if resp.status_code == 200:
-        resp = json.loads(resp.text)
-        albums = resp['items']
-        for album in albums:
-            if len(album['artists']) != 1:
-                print (album['name'])
-                artistsList = [artist['name'] for artist in album['artists']]
-                print (artistsList)
+    resp = requests.get('https://api.spotify.com/v1/artists/1mYsTxnqsietFxj1OgoGbG/albums', headers=headers, params=data)
+    while canProceed:
+        if resp.status_code == 200:
+            resp = json.loads(resp.text)
+            canProceed = (resp['next'] != None)
+            albums = resp['items']
+            for album in albums:
+                if len(album['artists']) != 1:
+                    print (album['name'])
+                    artistsList = [artist['name'] for artist in album['artists']]
+                    print (artistsList)
+        else:
+            canProceed = False
+            print (resp.text) # error message
 
 go()
