375236aa4e0e2c96f9603c13c2883b42e0c36548
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2019-03-05 17:17:44 +0530

    pass artists list to find_link.html

--- application.py
+++ application.py
@@ -5,19 +5,16 @@ application = Flask(__name__)
 
 g = SpotifyGraph.makeSpotifyGraph('OUTPUT_DATA.txt')
 
+artists = [n.nodeVal for n in g.nodesList.values() if n.nodeType == 'Artist']
+
 @application.route('/')
 def home():
     return render_template('home.html')
 
 @application.route('/findlink')
 def findLink():
-    return render_template('find_link.html')
+    return render_template('find_link.html', artists=artists)
 
-@application.route('/search')
-def searchArtist():
-    pass
 
 if __name__ == "__main__":
-    # uncomment the below line to run some sample query
-    # print ([f.nodeVal for f in g.run('Sanjay Joseph', 'Shankar Mahadevan')])
     application.run(debug=True)
